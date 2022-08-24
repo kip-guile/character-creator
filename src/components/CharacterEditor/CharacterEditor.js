08d6835663f554cfd587ef1c6a92fafb8940151e
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2022-08-24 23:57:46 +0100

    feat: done

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -25,6 +25,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspective}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -71,17 +72,17 @@ function App() {
             handleSelectOption={setClothesColor}
           />
         </div>
-        <div className={styles.characterWrapper}>
-          <Character
-            body={body}
-            head={head}
-            face={face}
-            accessory={accessory}
-            skinColor={skinColor}
-            clothesColor={clothesColor}
-          />
-        </div>
       </MaxWidthWrapper>
+      <div className={styles.characterWrapper}>
+        <Character
+          body={body}
+          head={head}
+          face={face}
+          accessory={accessory}
+          skinColor={skinColor}
+          clothesColor={clothesColor}
+        />
+      </div>
     </main>
   )
 }
